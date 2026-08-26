3c1f4d01da74570267aa55beefc2cf85be731144
Andrey <andreidetona@gmail.com>
2026-08-26 20:00:09 -0300

    Add files via upload

---
+++ Exercicio_2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Pessoa
+{
+    public string Nome;
+}
+
+class Program
+{
+    static void Main()
+    {
+        Pessoa pessoa1 = new Pessoa();
+        pessoa1.Nome = "João";
+
+        Pessoa pessoa2 = pessoa1;
+
+        pessoa2.Nome = "Carlos";
+
+        Console.WriteLine("Nome da pessoa1: " + pessoa1.Nome);
+    }
+}
