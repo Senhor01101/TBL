bf2200efebf70821f0bb5834a9917ea43bb5411a
Andrey <andreidetona@gmail.com>
2026-08-26 20:03:30 -0300

    Add files via upload

---
+++ Exercício_3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double valor = 15.78;
+
+        int valorInteiro = (int)valor;
+
+        Console.WriteLine("Valor original: " + valor);
+        Console.WriteLine("Valor após a conversão: " + valorInteiro);
+    }
+}
